388c7f7ef03071f8a4f6cb1a70eb200afb8d564a
ezio008 <lorenzoberzosa94@gmail.com>
2023-05-20 10:59:42 +0200

    Agregar archivos de proyecto.

---
+++ Skinet.API/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Skinet.Core.Entities;
+using Skinet.Infrastructure;
+
+namespace Skinet.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProductsController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _dbContext.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            if(product is null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(product);
+        }
+    }
+}
